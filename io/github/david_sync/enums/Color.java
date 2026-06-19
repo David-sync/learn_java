5f966cd77447aa79f0c571ee5488119ba2be7cec
David <leduc4852@gmail.com>
2026-06-19 23:11:36 +0700

    Enum_study_1

---
+++ io/github/david_sync/enums/Color.java
@@ -0,0 +1,20 @@
+package io.github.david_sync.enums;
+
+public enum Color {
+    RED("red"),
+    GREEN("green"),
+    BLUE("blue");
+
+    private String value;
+
+    Color(String value)
+    {
+        this.value = value;
+    }
+
+    public String getValue()
+    {
+        return value;
+    }
+
+}
